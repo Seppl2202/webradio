219c394a7b4c4a332bf42d15cd5dfddb7abf9425
Sebastian Schwegler <privat@sschwegler.de>
2018-07-02 21:09:00 +0200

    added network connectivity check information dialog

--- webradio-application/src/main/java/de/dhbw/webradio/networkconnection/NetworkConnectivityChecker.java
+++ webradio-application/src/main/java/de/dhbw/webradio/networkconnection/NetworkConnectivityChecker.java
@@ -2,6 +2,7 @@ package de.dhbw.webradio.networkconnection;
 
 import de.dhbw.webradio.gui.GUIHandler;
 import de.dhbw.webradio.gui.Gui;
+import de.dhbw.webradio.logger.Logger;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -16,9 +17,12 @@ import java.util.concurrent.*;
 
 public class NetworkConnectivityChecker {
     private static NetworkConnectivityChecker networkConnectivityChecker = new NetworkConnectivityChecker();
+    private int callCount = 0;
+    private JDialog checkInfoDialog;
 
     private NetworkConnectivityChecker() {
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        showInitialCheckDialog();
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
         ScheduledFuture scheduledFuture = scheduledExecutorService.schedule(new Callable<Object>() {
             public Object call() throws Exception {
                 checkNetworkConnectivity();
@@ -54,6 +58,8 @@ public class NetworkConnectivityChecker {
         Socket socket = null;
         boolean netAccess = false;
         try {
+            Logger.logInfo("checking network connection for the " + (callCount + 1) + " time");
+            callCount++;
             socket = new Socket("www.google.com", 80);
             netAccess = socket.isConnected();
             socket.close();
@@ -65,12 +71,32 @@ public class NetworkConnectivityChecker {
         } finally {
         }
         if (!netAccess) {
+            checkInfoDialog.dispose();
             showErrorDialog();
             GUIHandler.getInstance().toggleControls(false);
         } else {
+            checkInfoDialog.dispose();
             GUIHandler.getInstance().toggleControls(true);
         }
         return netAccess;
+    }
+
+    private void showInitialCheckDialog() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("entered method");
+                checkInfoDialog = new JDialog();
+                checkInfoDialog.setLocationRelativeTo(Gui.getInstance());
+                checkInfoDialog.setTitle("Prüfung");
+                checkInfoDialog.add(new JLabel("Prüfe Netzwerkverbindung..."));
+                checkInfoDialog.setModal(true);
+                checkInfoDialog.setVisible(true);
+                checkInfoDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+                checkInfoDialog.pack();
+            }
+        }).start();
+
     }
 
     private void showErrorDialog() {
